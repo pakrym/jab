4c85578e1e7b85af79921ce46eaa2b67966aaf32
Pavel Krymets <pavel@krymets.com>
2020-12-25 11:46:19 -0800

    remove presonal data from launchSettings.json

--- eng/CreateLaunchSettings.ps1
+++ eng/CreateLaunchSettings.ps1
@@ -1,18 +1,32 @@
-param($source, $launchSettings)
+param($source, $target)
 
 $settings = @{
         'profiles' = [ordered]@{}
 };
 
+$launchSettings = Join-Path $target "Properties" "launchSettings.json"
 $args = Get-Content $source
 $args[1] = '"' + $args[1] + '"'
+$joinedArgs = $args | Join-String -Separator ' ';
+$environmentVariablesToReplace = 'USERPROFILE', 'ProgramW6432'
+
+foreach ($ev in $environmentVariablesToReplace)
+{
+    $variableValue = (Get-Item "env:$ev").Value
+    $joinedArgs = $joinedArgs.Replace($variableValue, "`$($ev)")
+}
+
+$testProjectLocation = Get-Location
+
+pushd $target
+$relativePath = Resolve-Path -Relative $testProjectLocation
+popd
 
 $settings.profiles["csc"] = [ordered]@{
     'commandName'='Executable';
     'executablePath'='dotnet'
-    'commandLineArgs'= $args | Join-String -Separator ' ';
-    'workingDirectory'=Get-Location
+    'commandLineArgs'= $joinedArgs;
+    'workingDirectory'="`$(MSBuildProjectDirectory)\$relativePath"
 }
 
-Write-Host "dotnet " ($args | Join-String -Separator ' ')
 $settings | ConvertTo-Json | Out-File $launchSettings
