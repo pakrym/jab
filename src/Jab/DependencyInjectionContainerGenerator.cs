bc84476bc47b266689c36d7f8c27cc6e3d4738bb
Pavel Krymets <pavel@krymets.com>
2020-12-28 10:24:19 -0800

    class per file

--- src/Jab/DependencyInjectionContainerGenerator.cs
+++ src/Jab/DependencyInjectionContainerGenerator.cs
@@ -6,13 +6,6 @@ using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Jab
 {
-    internal static class DiagnosticDescriptors
-    {
-        public static readonly DiagnosticDescriptor UnexpectedErrorDescriptor = new DiagnosticDescriptor("JAB0001",
-            "Unexpected error during generation",
-            "Unexpected error occurred during code generation: {0}", "Usage", DiagnosticSeverity.Error, true);
-    }
-
     [Generator]
     #pragma warning disable RS1001 // We don't want this to be discovered as analyzer but it simplifies testing
     public class DependencyInjectionContainerGenerator : DiagnosticAnalyzer, ISourceGenerator
---
+++ src/Jab/DiagnosticDescriptors.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Jab
+{
+    internal static class DiagnosticDescriptors
+    {
+        public static readonly DiagnosticDescriptor UnexpectedErrorDescriptor = new DiagnosticDescriptor("JAB0001",
+            "Unexpected error during generation",
+            "Unexpected error occurred during code generation: {0}", "Usage", DiagnosticSeverity.Error, true);
+    }
+}
