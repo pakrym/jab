4229afb445afe5d1c523468916b34d4236be4fe9
Pavel Krymets <pavel@krymets.com>
2021-11-05 10:50:56 -0700

    Add module sample

---
+++ src/samples/ModuleSample/IModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Jab;
+
+namespace ModuleSample
+{
+    [ServiceProviderModule]
+    [Singleton(typeof(ServiceDefinedInAModule))]
+    public interface IModule
+    {
+    }
+}
