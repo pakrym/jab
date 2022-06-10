5160beae1e74862f42f5834976c031433d5cb097
Pavel Krymets <pavel@krymets.com>
2022-06-09 20:18:11 -0700

    Update package version and add description (#102)

--- eng/UpdateAllVersions.ps1
+++ eng/UpdateAllVersions.ps1
@@ -1,12 +1,24 @@
 param($NewVersion, $BasePath = "$PSScriptRoot/..")
 
+function Replace-In-File($file, $prefix, $regex)
+{
+    $content = Get-Content $file -Raw
+    if ($content -match $regex) {
+        Write-Host "Replacing text in file '$file'"
+        Set-Content $file -Value ($content -replace $matches[0].Trim(), "$prefix$NewVersion") -Force -NoNewline
+    }
+}
+
 $regexPrefix = "<PackageReference Include=`"Jab`" Version=`""
 $regexToFind = "$regexPrefix([\w\d\.-]+)"
 
 Get-ChildItem -Path $BasePath -File -Recurse | ForEach-Object {
-    $content = $_ | Get-Content -Raw
-    if ($content -match $regexToFind) {
-        Write-Host "Replacing text in file '$($_.FullName)'"
-        $_ | Set-Content -Value ($content -replace $matches[0].Trim(), "$regexPrefix$NewVersion") -Force -NoNewline
-    }
+    Replace-In-File $_.FullName $regexPrefix $regexToFind
 }
+
+$projectPath = Join-Path $BasePath "src/Jab/Jab.Common.props"
+$project = Get-Content $projectPath -Raw 
+$projectRegexPrefix = "<Version>"
+$projectRegexToFind = "$projectRegexPrefix([\w\d\.]+)"
+Replace-In-File $projectPath $projectRegexPrefix $projectRegexToFind
+
