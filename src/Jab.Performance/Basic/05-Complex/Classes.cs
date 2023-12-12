55fd77778a66171342d1c827bb3ec7c32870aa84
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2023-12-11 20:39:28 -0800

    Add ComplexBenchmark

---
+++ src/Jab.Performance/Basic/05-Complex/Classes.cs
@@ -0,0 +1,194 @@
+﻿namespace Jab.Performance.Basic.Complex;
+
+using Jab.Performance.Basic.Mixed;
+using Jab.Performance.Basic.Singleton;
+using Jab.Performance.Basic.Transient;
+
+public interface IComplex1
+{
+    void SayHi();
+}
+public interface IComplex2
+{
+    void SayHi();
+}
+public interface IComplex3
+{
+    void SayHi();
+}
+
+public interface IService1
+{
+    void SayHi();
+}
+public interface IService2
+{
+    void SayHi();
+}
+public interface IService3
+{
+    void SayHi();
+}
+
+public class Service1(ITransient1 Transient1) : IService1
+{
+    public void SayHi()
+    {
+        Transient1.SayHi();
+        Console.WriteLine("Hello from Service 1");
+    }
+}
+public class Service2(ITransient2 Transient2) : IService2
+{
+    public void SayHi()
+    {
+        Transient2.SayHi();
+        Console.WriteLine("Hello from Service 2");
+    }
+}
+public class Service3(ITransient3 Transient3) : IService3
+{
+    public void SayHi()
+    {
+        Transient3.SayHi();
+        Console.WriteLine("Hello from Service 3");
+    }
+}
+
+public class Complex1 : IComplex1
+{
+    private readonly IService1 _service1;
+    private readonly IService2 _service2;
+    private readonly IService3 _service3;
+    private readonly IMix1 _mix1;
+    private readonly IMix2 _mix2;
+    private readonly IMix3 _mix3;
+    private readonly ISingleton1 _singleton1;
+    private readonly ITransient1 _transient1;
+
+    public Complex1(IService1 service1, IService2 service2, IService3 service3, IMix1 mix1, IMix2 mix2, IMix3 mix3, ISingleton1 singleton1, ITransient1 transient1)
+    {
+        ArgumentNullException.ThrowIfNull(service1);
+        ArgumentNullException.ThrowIfNull(service2);
+        ArgumentNullException.ThrowIfNull(service3);
+        ArgumentNullException.ThrowIfNull(mix1);
+        ArgumentNullException.ThrowIfNull(mix2);
+        ArgumentNullException.ThrowIfNull(mix3);
+        ArgumentNullException.ThrowIfNull(singleton1);
+        ArgumentNullException.ThrowIfNull(transient1);
+
+        _service1 = service1;
+        _service2 = service2;
+        _service3 = service3;
+        _mix1 = mix1;
+        _mix2 = mix2;
+        _mix3 = mix3;
+        _singleton1 = singleton1;
+        _transient1 = transient1;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hello from Complex 1");
+        this._service1.SayHi();
+        this._service2.SayHi();
+        this._service3.SayHi();
+        this._mix1.SayHi();
+        this._mix2.SayHi();
+        this._mix3.SayHi();
+        this._singleton1.SayHi();
+        this._transient1.SayHi();
+    }
+}
+
+public class Complex2 : IComplex2
+{
+    private readonly IService1 _service1;
+    private readonly IService2 _service2;
+    private readonly IService3 _service3;
+    private readonly IMix1 _mix1;
+    private readonly IMix2 _mix2;
+    private readonly IMix3 _mix3;
+    private readonly ISingleton2 _singleton2;
+    private readonly ITransient2 _transient2;
+
+    public Complex2(IService1 service1, IService2 service2, IService3 service3, IMix1 mix1, IMix2 mix2, IMix3 mix3, ISingleton2 singleton2, ITransient2 transient2)
+    {
+        ArgumentNullException.ThrowIfNull(service1);
+        ArgumentNullException.ThrowIfNull(service2);
+        ArgumentNullException.ThrowIfNull(service3);
+        ArgumentNullException.ThrowIfNull(mix1);
+        ArgumentNullException.ThrowIfNull(mix2);
+        ArgumentNullException.ThrowIfNull(mix3);
+        ArgumentNullException.ThrowIfNull(singleton2);
+        ArgumentNullException.ThrowIfNull(transient2);
+
+        _service1 = service1;
+        _service2 = service2;
+        _service3 = service3;
+        _mix1 = mix1;
+        _mix2 = mix2;
+        _mix3 = mix3;
+        _singleton2 = singleton2;
+        _transient2 = transient2;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hello from Complex 2");
+        this._service1.SayHi();
+        this._service2.SayHi();
+        this._service3.SayHi();
+        this._mix1.SayHi();
+        this._mix2.SayHi();
+        this._mix3.SayHi();
+        this._singleton2.SayHi();
+        this._transient2.SayHi();
+    }
+}
+
+public class Complex3 : IComplex3
+{
+    private readonly IService1 _service1;
+    private readonly IService2 _service2;
+    private readonly IService3 _service3;
+    private readonly IMix1 _mix1;
+    private readonly IMix2 _mix2;
+    private readonly IMix3 _mix3;
+    private readonly ISingleton3 _singleton3;
+    private readonly ITransient3 _transient3;
+
+    public Complex3(IService1 service1, IService2 service2, IService3 service3, IMix1 mix1, IMix2 mix2, IMix3 mix3, ISingleton3 singleton3, ITransient3 transient3)
+    {
+        ArgumentNullException.ThrowIfNull(service1);
+        ArgumentNullException.ThrowIfNull(service2);
+        ArgumentNullException.ThrowIfNull(service3);
+        ArgumentNullException.ThrowIfNull(mix1);
+        ArgumentNullException.ThrowIfNull(mix2);
+        ArgumentNullException.ThrowIfNull(mix3);
+        ArgumentNullException.ThrowIfNull(singleton3);
+        ArgumentNullException.ThrowIfNull(transient3);
+
+        _service1 = service1;
+        _service2 = service2;
+        _service3 = service3;
+        _mix1 = mix1;
+        _mix2 = mix2;
+        _mix3 = mix3;
+        _singleton3 = singleton3;
+        _transient3 = transient3;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hello from Complex 3");
+        this._service1.SayHi();
+        this._service2.SayHi();
+        this._service3.SayHi();
+        this._mix1.SayHi();
+        this._mix2.SayHi();
+        this._mix3.SayHi();
+        this._singleton3.SayHi();
+        this._transient3.SayHi();
+    }
+}
---
+++ src/Jab.Performance/Basic/05-Complex/ComplexBenchmark.cs
@@ -0,0 +1,96 @@
+﻿namespace Jab.Performance.Basic.Complex; 
+
+using BenchmarkDotNet.Attributes;
+using Jab.Performance.Basic.Mixed;
+using Jab.Performance.Basic.Scoped;
+using Jab.Performance.Basic.Singleton;
+using Jab.Performance.Basic.Transient;
+using Microsoft.Extensions.DependencyInjection;
+using MEDI = Microsoft.Extensions.DependencyInjection;
+
+
+[MemoryDiagnoser]
+public class ComplexBenchmark
+{
+    private readonly MEDI.ServiceProvider _provider;
+    private readonly ContainerComplex _container = new();
+
+    public ComplexBenchmark()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddScoped<IComplex1, Complex1>();
+        serviceCollection.AddScoped<IComplex2, Complex2>();
+        serviceCollection.AddScoped<IComplex3, Complex3>();
+        serviceCollection.AddTransient<IService1, Service1>();
+        serviceCollection.AddTransient<IService2, Service2>();
+        serviceCollection.AddTransient<IService3, Service3>();
+        serviceCollection.AddTransient<IMix1, Mix1>();
+        serviceCollection.AddTransient<IMix2, Mix2>();
+        serviceCollection.AddTransient<IMix3, Mix3>();
+        serviceCollection.AddTransient<ITransient1, Transient1>();
+        serviceCollection.AddTransient<ITransient2, Transient2>();
+        serviceCollection.AddTransient<ITransient3, Transient3>();
+        serviceCollection.AddSingleton<ISingleton1, Singleton1>();
+        serviceCollection.AddSingleton<ISingleton2, Singleton2>();
+        serviceCollection.AddSingleton<ISingleton3, Singleton3>();
+        _provider = serviceCollection.BuildServiceProvider();
+    }
+
+    [Params(1, 10, 100)]
+    public int NumbersOfCalls { get; set; }
+
+    [Params(1, 2, 3)]
+    public int NumbersOfClasses { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public void Jab()
+    {
+        for (var i = 0; i < NumbersOfCalls; i++)
+        {
+            using var scope = _container.CreateScope();
+
+            if (NumbersOfClasses >= 1)
+                scope.GetService<IComplex1>();
+            if (NumbersOfClasses >= 2)
+                scope.GetService<IComplex2>();
+            if (NumbersOfClasses >= 3)
+                scope.GetService<IComplex3>();
+        }
+    }
+
+    [Benchmark]
+    public void MEDI()
+    {
+        for (var i = 0; i < NumbersOfCalls; i++)
+        {
+            using var scope = _provider.CreateScope();
+
+            if (NumbersOfClasses >= 1)
+                scope.ServiceProvider.GetService<IComplex1>();
+            if (NumbersOfClasses >= 2)
+                scope.ServiceProvider.GetService<IComplex2>();
+            if (NumbersOfClasses >= 3)
+                scope.ServiceProvider.GetService<IComplex3>();
+        }
+    }
+}
+
+[ServiceProvider]
+[Scoped(typeof(IComplex1), typeof(Complex1))]
+[Scoped(typeof(IComplex2), typeof(Complex2))]
+[Scoped(typeof(IComplex3), typeof(Complex3))]
+[Transient(typeof(IService1), typeof(Service1))]
+[Transient(typeof(IService2), typeof(Service2))]
+[Transient(typeof(IService3), typeof(Service3))]
+[Transient(typeof(IMix1), typeof(Mix1))]
+[Transient(typeof(IMix2), typeof(Mix2))]
+[Transient(typeof(IMix3), typeof(Mix3))]
+[Transient(typeof(ITransient1), typeof(Transient1))]
+[Transient(typeof(ITransient2), typeof(Transient2))]
+[Transient(typeof(ITransient3), typeof(Transient3))]
+[Singleton(typeof(ISingleton1), typeof(Singleton1))]
+[Singleton(typeof(ISingleton2), typeof(Singleton2))]
+[Singleton(typeof(ISingleton3), typeof(Singleton3))]
+internal partial class ContainerComplex
+{
+}
